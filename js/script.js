6b99e08dd1641a4527f883f1370764058f93beaa
Philippe-Guillaume Losembe <pglosembe@gmail.com>
2015-10-27 00:44:15 -0400

    Setting up youtube api script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+// Load YouTube IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// Create both player iframes 
+var leftPlayer, rightPlayer;
+function onYoutubeIframeAPIReady() {
+	leftPlayer = new YT.Player('leftPlayer', {
+		// config leftPlayer
+	});
+	rightPlayer = new YT.Player('rightPlayer', {
+		// config rightPlayer
+	});
+}
+
+/* Play button
+// Acts on both videos
+*/
 function playVideos() {
     var p = document.createElement('p');
     var t = document.createTextNode("Playing!");
