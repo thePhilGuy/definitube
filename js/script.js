1c4bea1af546158fa9d0433b9157f571a7d985f6
Philippe-Guillaume Losembe <pglosembe@gmail.com>
2015-10-27 00:26:41 -0400

    Added script

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function playVideos() {
+    var p = document.createElement('p');
+    var t = document.createTextNode("Playing!");
+    p.appendChild(t);
+    document.body.appendChild(p);
+    alert("haaaa");
+}
